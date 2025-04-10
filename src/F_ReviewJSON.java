6c3b9fd8aadcb6f071f06930d14aed9081b086c7
Jason <jason_yu25@milton.edu>
2025-04-10 11:36:53 -0400

    Initial commit

---
+++ src/F_ReviewJSON.java
@@ -0,0 +1,76 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class F_ReviewJSON {
+
+    public static void main(String[] args) throws ParseException {
+        F_ReviewJSON pokemon = new F_ReviewJSON();
+    }
+
+    public F_ReviewJSON() throws ParseException {
+        pull();
+    }
+
+    public void pull() throws ParseException {
+        String output = "";
+        String jsonString="";
+        try {
+
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/pikachu/"); /** Your API's URL goes here */
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                //System.out.println(output);
+                jsonString += output;
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // turn your string into a JSON object using a parser
+        JSONParser parser = new JSONParser();
+        JSONObject pikachu = (JSONObject) parser.parse(jsonString);
+        //System.out.println("Pikachu's JSON: " + pikachu);
+
+        /* TODO : print all of Pikachu's "moves" */
+        // feel free to paste this URL into your browser to explore the JSON: https://pokeapi.co/api/v2/pokemon/pikachu/
+
+        JSONArray moves = (JSONArray) pikachu.get("moves");
+        //System.out.println("Moves: " + moves);
+        for(int i=0; i < moves.size(); i++){
+            JSONObject element = (JSONObject) moves.get(i);
+            JSONObject move = (JSONObject) element.get("move");
+            String name = (String) move.get("name");
+            System.out.println("NAME OF MOVE: " + name);
+
+        }
+    }
+}
